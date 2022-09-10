6a0e23b2db7f3a5888552c95899363cd8fe9c92c
toMilanesi <tomi.milanesi@gmail.com>
2022-09-10 19:42:37 -0300

    Basic figure shapes

--- gosu-tutorial/window_demo.rb
+++ gosu-tutorial/window_demo.rb
@@ -4,6 +4,16 @@ require 'gosu'
 class DemoWindow < Gosu::Window
   def initialize
     super(640, 400, false)
+    self.caption = 'Gosu Demo Window'
+  end
+
+  def draw
+    x = 300
+    y = 200
+    size = 100
+    # draw_quad(x - size, y - size, 0xffff8888, x + size, y - size, 0xffffffff, x - size, y + size, 0xffffffff, x + size, y + size, 0xffffffff)
+    # draw_triangle(x - size, y - size, 0xff00ff00, x + size, y - size, 0xff00ff00, x + size, y + size, 0xff00ff00)
+    draw_line(x - size, y - size, 0xff00ffff, x + size, y - size, 0xff00ffff)
   end
 end
 
